da3f4106d5969f24bf0b1b523fa2889476dc1920
Ne-Mobu-u-Ne-ToNu <Seriy.sery2014@gmail.com>
2024-08-17 12:25:25 +0300

    add MouseListener

---
+++ src/main/java/org/example/ML.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ML extends MouseAdapter implements MouseListener {
+    private boolean isPressed;
+    private double x, y;
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        isPressed = true;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        isPressed = false;
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public boolean isPressed() {
+        return this.isPressed;
+    }
+}
